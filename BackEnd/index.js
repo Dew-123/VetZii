12768139c5e3a5c9a49748f8767937065d576c5e
Risandu Dewmika Disanayake <128276406+Dew-123@users.noreply.github.com>
2024-02-26 14:57:18 +0530

    Start the server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const { connectToMongoDB, getDataUsers ,addDataUsers,getDataVets,addDataVets } = require('./dataBase');
 const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
