b36a72f4c6de343265dcb06b73d5509c12807e37
Risandu Dewmika Disanayake <128276406+Dew-123@users.noreply.github.com>
2024-02-26 15:05:10 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -30,6 +30,61 @@ app.post('/dataGetUser', async (req, res) => {
   }
 });
 
+app.post('/dataAddUser', async (req, res) => {
+    try {
+      // Retrieve the data parameters from the request
+      const { Fname, Lname, nameOfThePet, petType, gender, email, mobileNumber, password } = req.body;
+  
+      // Log the received data
+      console.log('Received data:');
+      console.log('First Name:', Fname);
+      console.log('Last Name:', Lname);
+      console.log('Name of the Pet:', nameOfThePet);
+      console.log('Pet Type:', petType);
+      console.log('Gender:', gender);
+      console.log('Email:', email);
+      console.log('Mobile Number:', mobileNumber);
+      console.log('Password:', password);
+  
+      // Validate if all required fields are provided
+      if (!Fname || !Lname || !nameOfThePet || !petType || !gender || !email || !mobileNumber || !password) {
+        return res.status(400).json({ error: 'Missing required fields' });
+      }
+  
+      // Connect to MongoDB
+      await connectToMongoDB();
+  
+      // Check if the data already exists
+      const existingData = await getDataUsers(email,password);
+      console.log('Existing data length:', existingData.length);
+      if (existingData.length != 0) {
+        return res.status(400).json({ error: 'Data already exists' });
+      }
+  
+      // Prepare the data object
+      const newData = {
+        Fname,
+        Lname,
+        nameOfThePet,
+        petType,
+        gender,
+        email,
+        mobileNumber,
+        password
+      };
+  
+      // Add data to MongoDB
+      const result = await addDataUsers(newData);
+  
+      // Send the result as response
+      res.json({ message: 'Data added successfully', insertedId: result.insertedId });
+    } catch (error) {
+      console.error('Error handling API request:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
