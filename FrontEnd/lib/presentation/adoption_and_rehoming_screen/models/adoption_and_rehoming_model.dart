0da2b7739a9bd1202ec5a0cc29f527b230e1408c
Risandu Dewmika Disanayake <128276406+Dew-123@users.noreply.github.com>
2024-03-02 21:37:27 +0530

    Update adoption_and_rehoming_model.dart

--- FrontEnd/lib/presentation/adoption_and_rehoming_screen/models/adoption_and_rehoming_model.dart
+++ FrontEnd/lib/presentation/adoption_and_rehoming_screen/models/adoption_and_rehoming_model.dart
@@ -1,22 +1,41 @@
+import 'package:mihan_s_application1/presentation/adoption_and_rehoming_screen/models/userprofile1_item_model.dart';
 import '../../../core/app_export.dart';
-import 'userprofile1_item_model.dart';
+import '../../../http_req/serverHandling.dart';
+import 'package:mihan_s_application1/http_req/petAdoptionServerHandling.dart';
 
 /// This class defines the variables used in the [adoption_and_rehoming_screen],
 /// and is typically used to hold data that is passed between different parts of the application.
 class AdoptionAndRehomingModel {
-  Rx<List<Userprofile1ItemModel>> userprofile1ItemList = Rx([
-    Userprofile1ItemModel(
-        userImage: ImageConstant.imgRectangle4235.obs,
-        userName: "Bruno".obs,
-        dogDetails: "German Shepherd \n5 months\nContact - 0715486235".obs),
-    Userprofile1ItemModel(
-        userImage: ImageConstant.imgRectangle4235148x109.obs,
-        userName: "Whisker".obs,
-        dogDetails: "Cat\n1 year\nContact - 0774567892".obs),
-    Userprofile1ItemModel(
+
+  Rx<List<Userprofile1ItemModel>> userprofile1ItemList = Rx([]);
+
+  Future<void> fetchDataFromDatabase() async {
+
+    List<dynamic> databaseData = await fetchFromDatabase();
+
+    userprofile1ItemList.value = [];
+
+    for (var data in databaseData) {
+
+      Userprofile1ItemModel userModel = (Userprofile1ItemModel(
         userImage: ImageConstant.imgRectangle42351.obs,
-        userName: "Abby".obs,
-        dogDetails:
-            "Beagle dog needs a home \n5 months\nContact - 0715486235".obs)
-  ]);
+        userName: data['name'].toString().obs,
+        contactInfo: data['contactNo'].toString().obs,
+        dogDetails: data['description'].toString().obs,
+      ));
+      userprofile1ItemList.value.add(userModel);
+    }
+  }
+
+  // Method to fetch data from your database
+  Future<List<dynamic>> fetchFromDatabase() async {
+    try {
+      List<dynamic> dataSet = await fetchPetsData();
+      return dataSet;
+    } catch (e) {
+      print("Error fetching vets data: $e");
+
+      return [];
+    }
+  }
 }
