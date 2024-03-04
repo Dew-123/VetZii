63ddea0739b2c49903a9649d7c147427332ca7d0
SakithDW <121445033+SakithDW@users.noreply.github.com>
2024-03-04 10:55:02 +0530

    Few Changes in map
    Not finalized

--- FrontEnd/lib/presentation/clinic_map_page/controller/clinic_map_controller.dart
+++ FrontEnd/lib/presentation/clinic_map_page/controller/clinic_map_controller.dart
@@ -1,5 +1,10 @@
 import 'package:mihan_s_application1/core/app_export.dart';
 import 'package:mihan_s_application1/presentation/clinic_map_page/models/clinic_map_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 /// A controller class for the ClinicMapPage.
 ///
