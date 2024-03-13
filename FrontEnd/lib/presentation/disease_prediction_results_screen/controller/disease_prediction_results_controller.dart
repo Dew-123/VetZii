f834404cd136acd24be37f2a99207dc2447bc0f9
Risandu Dewmika Disanayake <128276406+Dew-123@users.noreply.github.com>
2024-03-13 21:48:41 +0530

    Errors fixed

--- FrontEnd/lib/presentation/disease_prediction_results_screen/controller/disease_prediction_results_controller.dart
+++ FrontEnd/lib/presentation/disease_prediction_results_screen/controller/disease_prediction_results_controller.dart
@@ -1,10 +1,6 @@
 import 'package:mihan_s_application1/core/app_export.dart';
 import 'package:mihan_s_application1/presentation/disease_prediction_results_screen/models/disease_prediction_results_model.dart';
 
-/// A controller class for the DiseasePredictionResultsScreen.
-///
-/// This class manages the state of the DiseasePredictionResultsScreen, including the
-/// current diseasePredictionResultsModelObj
 class DiseasePredictionResultsController extends GetxController {
   Rx<DiseasePredictionResultsModel> diseasePredictionResultsModelObj =
       DiseasePredictionResultsModel().obs;
