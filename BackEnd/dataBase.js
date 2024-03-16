8b796977aa5a4a0f58fe91dda0be6f09a58894f3
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-03-16 21:16:29 +0530

    added getRecords function

--- BackEnd/dataBase.js
+++ BackEnd/dataBase.js
@@ -388,6 +388,31 @@ async function updateVetData(
   }
 }
 
+async function getRecords(docEmail) {
+  try {
+    // Connect to MongoDB
+    await connectToMongoDB();
+
+    // Access "appointment" database and "toAccept" collection
+    const database = client.db("appointment");
+    const toAcceptCollection = database.collection("toAccept");
+
+    // Retrieve all appointment data associated with the provided userEmail from toAccept collection
+    const records = await toAcceptCollection
+      .find({ vetEmail: docEmail })
+      .toArray();
+
+    if (records.length === 0) {
+      throw new Error("No appointments found.");
+    }
+
+    return records;
+  } catch (error) {
+    console.error("Error retrieving appointment data:", error);
+    throw error;
+  }
+}
+
 async function getAppointment(userEmail) {
   try {
     // Connect to MongoDB
@@ -443,4 +468,5 @@ module.exports = {
   updateUserData,
   updateVetData,
   getAppointment,
+  getRecords,
 };
